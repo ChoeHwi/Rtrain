622b5677cb86a878a2d95859a50f414cf67e151e
SnowCastle <snowcastle0420@gmail.com>
2020-10-16 19:12:06 +0900

    UI_Score

---
+++ Rtrain/Assets/Yukinojo/Script/UI_Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Score : MonoBehaviour
+{
+    private Text scoreText;
+    private int score = 0;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        ReStart();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ScoreUpdate(int fare)
+    {
+        score += fare;
+        scoreText.text = "" + score;
+    }
+
+    public void ReStart()
+    {
+        score = 0;
+        scoreText.text = "" + score;
+    }
+
+}
