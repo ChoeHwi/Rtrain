edbee413ac919b82a282eaf61a76181ac11a9f6f
Junya Ueno <ueno.junya@icloud.com>
2020-10-17 20:00:53 +0900

    Scoreに対して運賃が加算されるようにしました。 自分のシーンで確認お願い致します。

--- Rtrain/Assets/Junya/Scripts/Passenger.cs
+++ Rtrain/Assets/Junya/Scripts/Passenger.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Passenger : PassengerBase
 {
     [SerializeField] int m_fare;
+    GameObject Score;
 
     public override void PayMent()
     {
-        
+        Score = GameObject.Find("Score");
+        Score.GetComponent<UI_Score>().ScoreUpdate(m_fare);
     }
 }
