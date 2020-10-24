c6c982893e0c9bc4f57899d5f316007000c225c5
banakane <vangakusei@gmail.com>
2020-10-24 15:24:47 +0900

    サウンドオプションのプロトタイプ

---
+++ Rtrain/Assets/Mizunasi/MizuScripts/SoundSettingPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSettingPopup : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_SoundPanel;
+
+    public void SoundPanelPopup()
+    {
+        m_SoundPanel.SetActive(true);
+    }
+    public void SoundPanelPopdown()
+    {
+        m_SoundPanel.SetActive(false);
+    }
+}
