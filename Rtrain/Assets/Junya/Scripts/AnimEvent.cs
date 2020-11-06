9ce0fcb2bd3dd651111df59f1106f184c9e76a51
Junya Ueno <ueno.junya@icloud.com>
2020-11-06 20:40:44 +0900

    ｔ

--- Rtrain/Assets/Junya/Scripts/AnimEvent.cs
+++ Rtrain/Assets/Junya/Scripts/AnimEvent.cs
@@ -5,17 +5,16 @@ using UnityEngine.Playables;
 
 public class AnimEvent : MonoBehaviour
 {
-    private GameManager m_gameManager = new GameManager();
-    public GameObject m_trainDir;
+    [SerializeField] public GameManager m_gameManager;
+   // public GameObject m_trainDir;
 
     private void Start()
     {
-        m_trainDir = GameObject.Find("TrainDirector");
+        //m_trainDir = GameObject.Find("TrainDirector");
     }
 
     public void NextStation()
     {
-        Debug.Log("animationEvent発生だぽん");
         m_gameManager.nextStation = true;
         //m_trainDir.GetComponent<TimelinePlayer>().PauseTimeline();
     }
