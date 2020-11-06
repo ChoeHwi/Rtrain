64b40974b65421b6bde6b02ebdad173f54cb81c2
Junya Ueno <ueno.junya@icloud.com>
2020-11-06 19:26:08 +0900

    AnimationEvent設置にゃう

---
+++ Rtrain/Assets/Junya/Scripts/AnimEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimEvent : MonoBehaviour
+{
+    private GameManager m_gameManager = new GameManager();
+    public GameObject m_trainDir;
+
+    private void Start()
+    {
+        m_trainDir = GameObject.Find("TrainDirector");
+    }
+
+    public void NextStation()
+    {
+        m_gameManager.nextStation = true;
+        m_trainDir.GetComponent<TimelinePlayer>().PauseTimeline();
+    }
+}
