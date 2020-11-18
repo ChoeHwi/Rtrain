696f2ee45276062441d24a81b8dd67da4bcc9e22
Goriyasu <goriyasu1998@gmail.com>
2020-11-18 23:40:13 +0900

    デザイン適応、ステージセレクト適応

---
+++ Rtrain/Assets/Inoue/Script/TitlePanel2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlePanel2 : MonoBehaviour
+{
+    public Animator titleTrain;
+    [SerializeField] GameObject selectPanel;
+    private void Start()
+    {
+        TitleOn();
+    }
+    public void TitleOn()
+    {
+        this.gameObject.SetActive(true);
+        titleTrain.SetTrigger("Title");
+    }
+    public void TitleOff()
+    {
+        this.gameObject.SetActive(false);
+        selectPanel.SetActive(true);
+    }
+}
