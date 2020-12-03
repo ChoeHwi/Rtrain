3438ab5bc472a8de3de39dea3b1dd9dedb3b7992
ButterflyPea <you@example.com>
2020-12-03 17:21:29 +0900

    ComPrinerやった

---
+++ Rtrain/Assets/tsukahara/script/ComPriner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComPriner : MonoBehaviour
+{
+           
+    GameObject GameManager;
+    [SerializeField] int socre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="")
+        {
+            GameManager gameaMnager = GameManager.GetComponent <GameManager>();
+            //gameaMnager.addSocre(socre);
+        }
+    }
+}
